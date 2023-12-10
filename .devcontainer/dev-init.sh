344a4f183b795e49be5d65240ac3c479e8cab01f
Joseph Wagner <joseph.wagner@hpe.com>
2023-12-10 08:15:58 +0000

    config for backup fake zod

--- .devcontainer/dev-init.sh
+++ .devcontainer/dev-init.sh
@@ -1,8 +1,12 @@
 # .devcontainer/dev-init.sh
 #!/bin/bash
 
-# TODO: create fake ship backup
 # Add commands to set up your Urbit development environment
 # For example, start a fake Urbit ship
 echo "Starting fake Urbit ship..."
 urbit -F zod
+
+# Want a fresh fake zod?
+# rm -r zod
+# cp -r zod.new zod
+# ./urbit zod
